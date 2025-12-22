fe6649f4e388fcbaebdac512c87f3ae1fe6042c0
Ephemera <kwanghaualex24s@forward.edu.my>
2025-12-22 15:06:59 +0800

    Imported the implementations needed for firebase Auth email/password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.messaging)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
