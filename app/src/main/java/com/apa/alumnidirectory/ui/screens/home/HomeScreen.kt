ad34e5bbfd4208873f650d8f4fffa74ed4d2fef3
JeromeE <jeremyho885@gmail.com>
2025-12-29 12:08:48 +0800

    Added Home Screen and Skeleton for loading

--- app/src/main/java/com/apa/alumnidirectory/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/apa/alumnidirectory/ui/screens/home/HomeScreen.kt
@@ -1,21 +1,73 @@
 package com.apa.alumnidirectory.ui.screens.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.apa.alumnidirectory.ui.theme.Primary
 
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+    var search by remember { mutableStateOf("") }
+
+    Home()
+}
+
+@Composable
+fun Home(
+
+) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
     ) {
-        Text("Hello Home Screen")
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.2f)
+            ) {
+                //Search
+                //Filter
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                //Content here
+            }
+        }
+        FloatingActionButton(
+            onClick = {  },
+            containerColor = Primary,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(40.dp)
+        ) {
+            Icon(
+                Icons.Outlined.Person, ""
+            )
+        }
     }
 }
