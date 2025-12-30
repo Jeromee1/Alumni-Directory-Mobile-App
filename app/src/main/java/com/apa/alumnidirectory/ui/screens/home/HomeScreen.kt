79ec2a1ed09e6071a469c979a7279e392b83606b
JeromeE <jeremyho885@gmail.com>
2025-12-30 09:53:12 +0800

    Cleaned up temp data and imports

--- app/src/main/java/com/apa/alumnidirectory/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/apa/alumnidirectory/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.apa.alumnidirectory.ui.screens.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
@@ -26,15 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.apa.alumnidirectory.data.enums.PreferredContact
 import com.apa.alumnidirectory.data.model.customtextfield.FieldData
-import com.apa.alumnidirectory.data.model.user.ContactInfo
-import com.apa.alumnidirectory.data.model.user.Location
-import com.apa.alumnidirectory.data.model.user.UserData
-import com.apa.alumnidirectory.ui.components.core.HomeUserCard
 import com.apa.alumnidirectory.ui.components.inputs.CustomFilterButton
 import com.apa.alumnidirectory.ui.components.inputs.CustomTextField
 import com.apa.alumnidirectory.ui.theme.Primary
@@ -46,55 +38,13 @@ fun HomeScreen(
 ) {
     var search by remember { mutableStateOf("") }
 
-    val tempUsers = listOf(
-        UserData(
-            uid = "123",
-            fullName = "John Doe",
-            fullNameLower = "john doe",
-            email = "johndoe@gmail.com",
-            status = "approved",
-            role = "admin",
-            graduationYear = "2025",
-            department = "software stuff",
-            position = "doggy",
-            company = "SafeTruck",
-            primaryStack = "thaddeous",
-            location = Location(
-                "Kuala Lumpur", "Malaysia"
-            ),
-            preferredContact = PreferredContact.EMAIL.value,
-            contact = ContactInfo(),
-            bio = null,
-            photoUrl= null,
-        ),
-        UserData(
-            uid = "123",
-            fullName = "Jane Doe",
-            fullNameLower = "jane doe",
-            email = "janedoe@gmail.com",
-            status = "approved",
-            role = "user",
-            graduationYear = "2025",
-            department = "software stuff",
-            position = "CEO",
-            company = "Intel",
-            primaryStack = "Fullstack",
-            location = Location(
-                "Kuala Lumpur", "Malaysia"
-            ),
-            preferredContact = PreferredContact.EMAIL.value,
-            contact = ContactInfo(),
-            bio = null,
-            photoUrl= null,
-        )
-    )
 
-    Home(tempUsers, search) { search = it }
+    Home(/*tempUsers,*/ search) { search = it }
 }
 
 @Composable
 fun Home(
-    users: List<UserData>,
+//    users: List<UserData>,
     search: String,
     onSearchChange: (String) -> Unit
 ) {
@@ -133,13 +83,13 @@ fun Home(
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                items(users) {
-                    HomeUserCard(it) {  }
-                }
+//                items(users) {
+////                    HomeUserCard(it) { navController.navigate() }
+////                }
             }
         }
         FloatingActionButton(
-            onClick = {  },
+            onClick = { },
             containerColor = Primary,
             modifier = Modifier
                 .align(Alignment.BottomEnd)
