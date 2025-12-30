bb76ecc2d9ff39f2aef4fdae99151f72104931ee
Ephemera <kwanghaualex24s@forward.edu.my>
2025-12-30 15:22:25 +0800

    Changed AppNav.kt start location back to Login and moved NavHost out of Scaffold. AuthRepo and AuthRepoImpl now has fetchProfile function. Made CurrentUser.kt to house FirebaseUserData and the UserData. Added user StateFlow, updateUser(), getCurrentUser() to FirebaseAuthService.kt. Added a LaunchedEffect to Login to navigate after login is successful. Added a finish SharedFlow to LoginViewModel.kt. Created PendingViewModel.kt. Connected PendingScreen.kt and PendingViewModel.kt to the data layer.

--- app/src/main/java/com/apa/alumnidirectory/service/FirebaseAuthService.kt
+++ app/src/main/java/com/apa/alumnidirectory/service/FirebaseAuthService.kt
@@ -1,30 +1,54 @@
 package com.apa.alumnidirectory.service
 
 import android.util.Log
+import com.apa.alumnidirectory.data.model.user.FirebaseData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseUser
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.tasks.await
 
 class FirebaseAuthService @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) {
+    private val _user = MutableStateFlow<FirebaseData?>(null)
+    val user = _user.asStateFlow()
+
+    private fun updateUser(firebaseUser: FirebaseUser) {
+        firebaseUser.let { user ->
+            _user.update {
+                FirebaseData(
+                    uid = user.uid,
+                    email = user.email.toString(),
+                    isEmailVerified = user.isEmailVerified
+                )
+            }
+        }
+    }
+
+    fun getCurrentUser(): FirebaseData? {
+        return user.value
+    }
+
     suspend fun register(email: String, password: String): String {
         val result =
             try {
-                firebaseAuth.createUserWithEmailAndPassword(email, password)
-                    .await()
+                firebaseAuth.createUserWithEmailAndPassword(email, password).await()
             } catch (e: FirebaseAuthUserCollisionException) {
                 throw IllegalStateException("Email in-use")
             } catch (e: Exception) {
                 throw e
             }
+
         return result.user?.uid ?: throw IllegalStateException("User creation failed")
     }
 
     suspend fun login(email: String, password: String): FirebaseUser {
         val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+        result.user?.let { updateUser(it) }
 
         return result.user ?: throw java.lang.IllegalStateException("Login failed")
     }
--- app/src/main/java/com/apa/alumnidirectory/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/apa/alumnidirectory/ui/screens/login/LoginViewModel.kt
@@ -3,22 +3,30 @@ package com.apa.alumnidirectory.ui.screens.login
 import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.apa.alumnidirectory.data.model.auth.LoginReq
+import com.apa.alumnidirectory.data.model.user.UserData
 import com.apa.alumnidirectory.data.repo.AuthRepo
 import com.apa.alumnidirectory.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     val repo: AuthRepo
-): BaseViewModel() {
+) : BaseViewModel() {
+    private val _finish = MutableSharedFlow<Unit>()
+    val finish = _finish.asSharedFlow()
 
     fun login(loginReq: LoginReq) {
         viewModelScope.launch {
-            safeApiCall { repo.login(loginReq).let {
-                Log.d("debug", it.toString())
-            } }
+            safeApiCall {
+                repo.login(loginReq).let {
+                    Log.d("debug", it.toString())
+                    _finish.emit(Unit)
+                }
+            }
         }
     }
 }
