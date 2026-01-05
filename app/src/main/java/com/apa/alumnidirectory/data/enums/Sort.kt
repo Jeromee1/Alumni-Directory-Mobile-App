273cda7f244e364ce19947a1afdf4aa89eea808e
JeromeE <jeremyho885@gmail.com>
2026-01-05 16:06:58 +0800

    Added Filter and Sort enums

---
+++ app/src/main/java/com/apa/alumnidirectory/data/enums/Sort.kt
@@ -0,0 +1,7 @@
+package com.apa.alumnidirectory.data.enums
+
+enum class Sort(val value: String) {
+    NAME("Name A-Z"),
+    GRADUATION_YEAR("Graduation Year"),
+    RECENTLY_UPDATED("Recently Updated")
+}
