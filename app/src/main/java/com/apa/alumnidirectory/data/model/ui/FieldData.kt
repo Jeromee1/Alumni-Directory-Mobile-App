f0ae59a91a23ad2d6816b3fdbdacdb53a8e569d4
JeromeE <jeremyho885@gmail.com>
2026-01-12 16:14:04 +0800

    Idk why this didn't get pushed

--- app/src/main/java/com/apa/alumnidirectory/data/model/ui/FieldData.kt
+++ app/src/main/java/com/apa/alumnidirectory/data/model/ui/FieldData.kt
@@ -4,5 +4,6 @@ data class FieldData(
     val label: String,
     val value: String,
     val list: List<String> = emptyList(),
+    val isPassword: Boolean = false,
     val onValueChange: (String) -> Unit
 )
